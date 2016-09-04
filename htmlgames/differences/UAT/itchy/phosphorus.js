c5aa67e0795b0376202df03e705ce3295bfe919b
htmlgames <paul777@myway.com>
2016-09-04 17:41:34 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,4 +1,4 @@
-// additional bugfixes by PF. Please visit: goo.gl/zI6A (v0.010)
+// additional bugfixes by PF. Please visit: goo.gl/zI6A (v0.011)
 var that; // PF
 
 var P = (function() {
@@ -2741,9 +2741,21 @@ P.compile = (function() {
       }
 
       if (['turnRight:', 'turnLeft:', 'heading:', 'pointTowards:', 'setRotationStyle', 'lookLike:', 'nextCostume', 'say:duration:elapsed:from:', 'say:', 'think:duration:elapsed:from:', 'think:', 'changeGraphicEffect:by:', 'setGraphicEffect:to:', 'filterReset', 'changeSizeBy:', 'setSizeTo:', 'comeToFront', 'goBackByLayers:'].indexOf(block[0]) !== -1) {
-          source += 'if (S.visible) VISUAL = true;\n';
+          source += 'if (S.visible) "; //"VISUAL = true;\n';
+          // PF new block below - for show / hide trick some projects use (like manic miner)      
+	  if (that.bInProcDef) {
+        	  source += 'VISUAL = false;\n';
+	  } else {
+        	  source += 'VISUAL = true;\n';
+	  }          
       } else if (['forward:', 'gotoX:y:', 'gotoSpriteOrMouse:', 'changeXposBy:', 'xpos:', 'changeYposBy:', 'ypos:', 'bounceOffEdge', 'glideSecs:toX:y:elapsed:from:'].indexOf(block[0]) !== -1) {
-          source += 'if (S.visible || S.isPenDown) VISUAL = true;\n';
+          source += 'if (S.visible || S.isPenDown) "; //" VISUAL = true;\n';
+          // PF new block below - for show / hide trick some projects use (like manic miner)      
+	  if (that.bInProcDef) {
+        	  source += 'VISUAL = false;\n';
+	  } else {
+        	  source += 'VISUAL = true;\n';
+	  }          
       } else if (['showBackground:', 'startScene', 'nextBackground', 'nextScene', 'startSceneAndWait', 'show', 'hide', 'putPenDown', 'stampCostume', 'showVariable:', 'hideVariable:', 'doAsk', 'setVolumeTo:', 'changeVolumeBy:', 'setTempoTo:', 'changeTempoBy:'].indexOf(block[0]) !== -1) {
         // PF new block below - for show / hide trick some projects use (like manic miner)      
 	if (that.bInProcDef) {
