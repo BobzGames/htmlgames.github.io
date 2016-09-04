c09079cb6d5eab913e06304b24b40efc1959eb76
htmlgames <paul777@myway.com>
2016-09-04 20:37:53 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,4 +1,4 @@
-// additional bugfixes by PF. Please visit: goo.gl/zI6A (v0.041)
+// additional bugfixes by PF. Please visit: goo.gl/zI6A (v0.042)
 var that; // PF
 
 var P = (function() {
@@ -3318,7 +3318,7 @@ P.compile = (function() {
     var fns = [0];
 
     if (script[0][0] === 'procDef') {
-      that.bInProcDef = script[0][4]; //warp;
+      //that.bInProcDef = script[0][4]; //warp;
       var inputs = script[0][2];
       var types = script[0][1].match(/%[snmdcb]/g) || [];
       for (var i = types.length; i--;) {
@@ -3427,7 +3427,7 @@ P.compile = (function() {
       var key = script[0][1].toLowerCase();
       (object.listeners.whenSceneStarts[key] || (object.listeners.whenSceneStarts[key] = [])).push(f);
     } else if (script[0][0] === 'procDef') { 
-      //that.bInProcDef = script[0][4]; // pf1 - this enables a faster screen redraw (has side effects?)
+      that.bInProcDef = script[0][4]; // pf1 - this enables a faster screen redraw (has side effects?)
       //console.log("Run without screen refresh: " + script[0][4] + ", in Function Block:  " + script[0][1].split(" ")); // pf debug - dm
       object.procedures[script[0][1]] = {
         inputs: inputs,
