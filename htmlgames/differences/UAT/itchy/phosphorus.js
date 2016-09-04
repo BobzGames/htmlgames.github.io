aa5942dfd333cd2dc368ccb4dbbfdb39bf5d3577
htmlgames <paul777@myway.com>
2016-09-04 19:37:41 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,4 +1,4 @@
-// additional bugfixes by PF. Please visit: goo.gl/zI6A (v0.028)
+// additional bugfixes by PF. Please visit: goo.gl/zI6A (v0.029)
 var that; // PF
 
 var P = (function() {
@@ -2745,7 +2745,7 @@ P.compile = (function() {
       } else if (['forward:', 'gotoX:y:', 'gotoSpriteOrMouse:', 'changeXposBy:', 'xpos:', 'changeYposBy:', 'ypos:', 'bounceOffEdge', 'glideSecs:toX:y:elapsed:from:'].indexOf(block[0]) !== -1) {
           source += 'if (S.visible || S.isPenDown) VISUAL = true;\n';
       } else if (['showBackground:', 'startScene', 'nextBackground', 'nextScene', 'startSceneAndWait', 'show', 'hide', 'putPenDown', 'stampCostume', 'showVariable:', 'hideVariable:', 'doAsk', 'setVolumeTo:', 'changeVolumeBy:', 'setTempoTo:', 'changeTempoBy:'].indexOf(block[0]) !== -1) {
-          source += 'VISUAL = true;\n';
+          source += 'if (S.visible) VISUAL = true;\n'; // pf manic miner
       } else if (that.bInProcDef || window.location.href.match("102592962")) {
       	  // pf manic miner
       	  source += 'VISUAL = false;\n';
