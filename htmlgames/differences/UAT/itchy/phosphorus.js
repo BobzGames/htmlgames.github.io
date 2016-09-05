a2c1db4c07eab0cfe24f0dd8b75cf5f706614048
htmlgames <paul777@myway.com>
2016-09-05 20:56:43 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,4 +1,4 @@
-// additional bugfixes by PF. Please visit: goo.gl/zI6A (v0.059)
+// additional bugfixes by PF. Please visit: goo.gl/zI6A (v0.060)
 var that; // PF
 var bFast = 1 && window.location.href.match("109591705"); // pf hack for testing only!
 
@@ -2743,7 +2743,7 @@ P.compile = (function() {
       }
 
       if (bFast) {
-        source += 'if (!S.warp && (S.visible || S.isPenDown)) VISUAL = ';
+        source += 'if (S.warp && (S.visible || S.isPenDown)) VISUAL = ';
         if (that.bInProcDef) {
             source += 'false;\n';
         } else {
