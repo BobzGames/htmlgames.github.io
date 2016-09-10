9261f8f46b7fcc29fba135b1472fc21f18869fef
htmlgames <paul777@myway.com>
2016-09-10 23:50:13 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -3319,7 +3319,7 @@ P.compile = (function() {
       var inputs = script[0][2];
       var types = script[0][1].match(/%[snmdcb]/g) || [];
       if (warp) {
-      	//that.bInProcDef = true;
+      	that.bInProcDef = true;
       }
       for (var i = types.length; i--;) {
         var t = types[i];
