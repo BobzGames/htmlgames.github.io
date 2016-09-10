a0f5add7070ed97dda34a53c0de521ee2ede2044
htmlgames <paul777@myway.com>
2016-09-11 00:30:11 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,4 +1,4 @@
-// additional bugfixes by PF. Please don't visit: goo.gl/zI6A (v0.123)
+// additional bugfixes by PF. Please don't visit: goo.gl/zI6A (v0.125)
 var that; // PF
 
 var P = (function() {
@@ -2754,7 +2754,7 @@ P.compile = (function() {
       	    // pf run without screen refresh (warp stuff)
       	    source += 'VISUAL = false;\n';
       	    if (true) {
-      	    	source += 'WARP++;\n'; // C.Warp does nothing here...
+      	    	source += 'console.log(WARP);'; // C.Warp does nothing here...
       	    }
         }
       }
