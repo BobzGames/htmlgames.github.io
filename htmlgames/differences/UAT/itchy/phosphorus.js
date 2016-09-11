aa5632f06dcb9e1a0489e2a0c075218da822ff65
htmlgames <paul777@myway.com>
2016-09-11 02:15:45 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -2740,17 +2740,17 @@ P.compile = (function() {
         source += 'console.log(' + val(block[0]) + ');\n';
       }
       var bFast = 1 && window.location.href.match("117879477"); // 3D X-Wing
-      if (that.bWarp && that.bInProcDef) {
-	 // pf hack for testing only!
+      if (bFast && that.bInProcDef) {
+	 // pf hack for testing only! x
       } else {
       	// pf - S.visible is set true / false, depending if block is show / hide
         if (['turnRight:', 'turnLeft:', 'heading:', 'pointTowards:', 'setRotationStyle', 'lookLike:', 'nextCostume', 'say:duration:elapsed:from:', 'say:', 'think:duration:elapsed:from:', 'think:', 'changeGraphicEffect:by:', 'setGraphicEffect:to:', 'filterReset', 'changeSizeBy:', 'setSizeTo:', 'comeToFront', 'goBackByLayers:'].indexOf(block[0]) !== -1) {
-            source += 'if (S.visible) VISUAL = true\n'; // 1 0
+            source += 'if (S.visible) VISUAL = true\n'; // 1 0 x
         } else if (['forward:', 'gotoX:y:', 'gotoSpriteOrMouse:', 'changeXposBy:', 'xpos:', 'changeYposBy:', 'ypos:', 'bounceOffEdge', 'glideSecs:toX:y:elapsed:from:'].indexOf(block[0]) !== -1) {
             source += 'if (S.visible || S.isPenDown) VISUAL = true\n'; // 1 0
         } else if (['showBackground:', 'startScene', 'nextBackground', 'nextScene', 'startSceneAndWait', 'show', 'hide', 'putPenDown', 'stampCostume', 'showVariable:', 'hideVariable:', 'doAsk', 'setVolumeTo:', 'changeVolumeBy:', 'setTempoTo:', 'changeTempoBy:'].indexOf(block[0]) !== -1) {
-            source += 'VISUAL = true;\n'; // 1 0
-        } else if (that.bWarp && that.bInProcDef) {
+            source += 'VISUAL = true;\n'; // 1 0 i
+        } else if (!bFast && that.bInProcDef) {
       	    // pf run without screen refresh (warp stuff)
       	    source += 'VISUAL = false;\n'; // pf makes a small speed increase ?
       	    if (that.bWarp) {
