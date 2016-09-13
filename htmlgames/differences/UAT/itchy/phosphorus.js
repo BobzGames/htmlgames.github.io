3195852c17d03f5bfa161bcf23ffedff9b64615f
htmlgames <paul777@myway.com>
2016-09-13 23:39:37 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,4 +1,4 @@
-// additional bugfixes by PF... (v0.184!!)
+// additional bugfixes by PF... (v0.184!)
 var that; // PF
 
 var P = (function() {
@@ -2745,7 +2745,7 @@ P.compile = (function() {
           source += 'if (S.visible || S.isPenDown) VISUAL = true\n';
       } else if (['showBackground:', 'startScene', 'nextBackground', 'nextScene', 'startSceneAndWait', 'show', 'hide', 'putPenDown', 'stampCostume', 'showVariable:', 'hideVariable:', 'doAsk', 'setVolumeTo:', 'changeVolumeBy:', 'setTempoTo:', 'changeTempoBy:'].indexOf(block[0]) !== -1) {
           source += 'VISUAL = true;\n';
-      } else if (that.bInProcDef) {
+      } else if (that.bInProcDef||WARP) {
       	  // pf run without screen refresh (warp stuff)
       	  if (WARP) {
       	    	//source += 'VISUAL = false;\n'; // pf makes a small speed increase ?
