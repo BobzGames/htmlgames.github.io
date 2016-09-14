e1de82623399d1050f1d25cd9f31405faab2eaf6
htmlgames <paul777@myway.com>
2016-09-14 23:51:36 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itch/phosphorus.js
+++ htmlgames/differences/UAT/itch/phosphorus.js
@@ -1,4 +1,4 @@
-var P = (function() { // 0.011
+var P = (function() { // 0.014
   'use strict';
 
   var SCALE = window.devicePixelRatio || 1;
@@ -2528,7 +2528,7 @@ P.compile = (function() {
       }
 
       if (['turnRight:', 'turnLeft:', 'heading:', 'pointTowards:', 'setRotationStyle', 'lookLike:', 'nextCostume', 'say:duration:elapsed:from:', 'say:', 'think:duration:elapsed:from:', 'think:', 'changeGraphicEffect:by:', 'setGraphicEffect:to:', 'filterReset', 'changeSizeBy:', 'setSizeTo:', 'comeToFront', 'goBackByLayers:'].indexOf(block[0]) !== -1) {
-        source += 'if (S.visible) VISUAL = true;\n';
+        source += 'if (S.visible && !C.warp) VISUAL = true;\n';
       } else if (['forward:', 'gotoX:y:', 'gotoSpriteOrMouse:', 'changeXposBy:', 'xpos:', 'changeYposBy:', 'ypos:', 'bounceOffEdge', 'glideSecs:toX:y:elapsed:from:'].indexOf(block[0]) !== -1) {
         source += 'if (S.visible || S.isPenDown) VISUAL = true;\n';
       } else if (['showBackground:', 'startScene', 'nextBackground', 'nextScene', 'startSceneAndWait', 'show', 'hide', 'putPenDown', 'stampCostume', 'showVariable:', 'hideVariable:', 'doAsk', 'setVolumeTo:', 'changeVolumeBy:', 'setTempoTo:', 'changeTempoBy:'].indexOf(block[0]) !== -1) {
