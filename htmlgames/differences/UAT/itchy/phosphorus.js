32c7ee03b7e8316fdb009eb96bfa6aa9db6a2197
htmlgames <paul777@myway.com>
2016-09-14 21:08:21 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,4 +1,4 @@
-// additional bugfixes by PF... (v0.185)
+// additional bugfixes by PF... (v0.186)
 var that; // PF
 var TurboMode = false; // 100% compatibility for starters (use at your own risk!)
 
@@ -3446,7 +3446,7 @@ P.compile = (function() {
         };        
       } else {
       	// WE ARE IN NORMAL MODE!
-        that.bInProcDef = script[0][4];
+        that.bInProcDef = false; //script[0][4];
       	object.procedures[script[0][1]] = {
           inputs: inputs,
           warp: script[0][4],
