ce8848e0ac2ed4753d176f4884422f0fefba5563
htmlgames <paul777@myway.com>
2016-09-19 19:47:42 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,4 +1,4 @@
-// additional bugfixes by PF (v0.188+)
+// additional bugfixes by PF (v0.188++)
 var that; // PF
 var TurboMode = !!window.location.search.match("turbo=true"); // false = 99% compatibility for starters (use at your own risk!) 
 console.log("TurboMode: " + TurboMode);
@@ -2756,11 +2756,11 @@ P.compile = (function() {
       	  // pf run without screen refresh (warp stuff)
       	  if (TurboMode) {
       	      if (that.bWarp) {
-      	    	source += 'VISUAL = false;\n'; // pf makes a small speed increase ?
+      	    	//source += 'VISUAL = false;\n'; // pf makes a small speed increase ?
       	    	source += 'WARP = 1;\n'; // can cause 'lockup', note C.Warp does nothing here...
       	      }
       	  } else {
-      	        source += 'VISUAL = false;\n'; // pf makes a small speed increase ?	
+      	        //source += 'VISUAL = false;\n'; // pf makes a small speed increase ?	
       	  }
       }
 
@@ -4090,7 +4090,7 @@ P.runtime = (function() {
         for (var i = queue.length; i--;) {
           if (!queue[i]) queue.splice(i, 1);
         }
-      } while (Date.now() - start < 1000 / this.framerate && queue.length); // pf removed self.isTurbo || (!VISUAL) && 
+      } while ((!VISUAL) && Date.now() - start < 1000 / this.framerate && queue.length); // pf removed self.isTurbo ||  
       this.draw();
       S = null;
       // PF
