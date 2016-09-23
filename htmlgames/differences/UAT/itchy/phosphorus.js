7b90cabee8476b0d14150cbccc513bdc5e61d2e8
htmlgames <paul777@myway.com>
2016-09-23 23:21:19 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,4 +1,4 @@
-// additional bugfixes by PF (v0.189++)
+// additional bugfixes by PF (v0.190++)
 var that; // PF
 var TurboMode = !!window.location.search.match("turbo=true"); // false = 99% compatibility for starters (use at your own risk!) 
 console.log("TurboMode: " + TurboMode);
@@ -611,6 +611,35 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
   };
 
   IO.fixSVG = function(svg, element) {
+    if (element.nodeType !== 1) return;
+    if (element.nodeName === 'text') {
+      var bb = element.getBBox();
+      var x = 4 - (0.44 * element.transform.baseVal.consolidate().matrix.a);
+      var y = (element.getAttribute('y') - bb.y * 1.044); // pf svg text
+      element.setAttribute('x', x);
+      element.setAttribute('y', y);
+      var lines = element.textContent.split('\n');
+      if (lines.length > 1) {
+        element.textContent = lines[0];
+        var lineHeight = IO.LINE_HEIGHTS[font] || 1;
+        for (var i = 1, l = lines.length; i < l; i++) {
+          var tspan = document.createElementNS(null, 'tspan');
+          tspan.textContent = lines[i];
+          tspan.setAttribute('x', x);
+          tspan.setAttribute('y', y + size * i * lineHeight);
+          element.appendChild(tspan);
+        }
+      }
+      // svg.style.cssText = '';
+      // console.log(element.textContent, 'data:image/svg+xml;base64,' + btoa(svg.outerHTML));
+    } else if ((element.hasAttribute('x') || element.hasAttribute('y')) && element.hasAttribute('transform')) {
+      element.setAttribute('x', 0);
+      element.setAttribute('y', 0);
+    }
+    [].forEach.call(element.childNodes, IO.fixSVG.bind(null, svg));
+  };
+
+  IO.fixSVG_OLD = function(svg, element) {
     if (element.nodeType !== 1) return;
     if (element.nodeName === 'text') { 
       var font = element.getAttribute('font-family') || '';
