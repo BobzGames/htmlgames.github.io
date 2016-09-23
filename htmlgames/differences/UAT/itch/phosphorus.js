139dff56f1371c51a6bdb7ec525f15dc373a608d
htmlgames <paul777@myway.com>
2016-09-23 23:14:50 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itch/phosphorus.js
+++ htmlgames/differences/UAT/itch/phosphorus.js
@@ -474,7 +474,7 @@ var P = (function() {
 
       var bb = element.getBBox();
       var x = 4 - (0.44 * element.transform.baseVal.consolidate().matrix.a);
-      var y = (element.getAttribute('y') - bb.y * 1.040); // pf svg text 1.1
+      var y = (element.getAttribute('y') - bb.y * 1.044); // pf svg text 1.1
       element.setAttribute('x', x);
       element.setAttribute('y', y);
       var lines = element.textContent.split('\n');
