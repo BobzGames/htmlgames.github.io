638253b4fbf68db7477a235fbd238e3b3d804fc3
htmlgames <paul777@myway.com>
2016-09-23 23:37:17 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -613,8 +613,8 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
   IO.fixSVG = function(svg, element) {
     if (element.nodeType !== 1) return;
     if (element.nodeName === 'text') {
-      var size = +element.getAttribute('font-size');
-      element.setAttribute('font-size', size / 1.044);
+      var size = element.getAttribute('font-size');
+      element.setAttribute('font-size', size);
       var bb = element.getBBox();
       var x = 4 - (0.44 * element.transform.baseVal.consolidate().matrix.a);
       var y = (element.getAttribute('y') - bb.y * 1.044); // pf svg text
