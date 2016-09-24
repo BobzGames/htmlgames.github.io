e36da30cc52201d8fcdeaf788cb1fffaf1fdf1aa
htmlgames <paul777@myway.com>
2016-09-24 23:13:15 +0100

    Update canvg.js

--- htmlgames/differences/UAT/itchy/canvg.js
+++ htmlgames/differences/UAT/itchy/canvg.js
@@ -2149,7 +2149,8 @@
 				var x = this.attribute('x').toPixels('x');
 				var y = this.attribute('y').toPixels('y');
 				var fontSize = this.parent.style('font-size').numValueOrDefault(svg.Font.Parse(svg.ctx.font).fontSize);
-				return new svg.BoundingBox(x, y - fontSize, x + Math.floor(fontSize * 1.0 / 3.0) * this.children[0].getText().length, y);
+				//return new svg.BoundingBox(x, y - fontSize, x + Math.floor(fontSize * 2.0 / 3.0) * this.children[0].getText().length, y);
+				return new svg.BoundingBox(x, y - fontSize, x + Math.floor(fontSize * 2.0 / 3.0) * this.children[0].getText().length * 2, y);
 			}
 
 			this.renderChildren = function(ctx) {
