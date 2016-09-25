134c818f83faa5c3629acbb9d78b54d7d6285ffe
htmlgames <paul777@myway.com>
2016-09-25 23:51:01 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -182,6 +182,7 @@ var P = (function() {
 
   IO.FONTS = {
     '': 'Helvetica',
+    Scratch: 'Scratch',	  
     Donegal: 'Donegal One',
     Gloria: 'Gloria Hallelujah',
     Marker: 'Permanent Marker',
