4aa6fed0fb0cd5ad3f0bf137ea56360d1a45667c
htmlgames <paul777@myway.com>
2016-09-25 22:39:14 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itch/phosphorus.js
+++ htmlgames/differences/UAT/itch/phosphorus.js
@@ -526,10 +526,9 @@ var P = (function() {
         document.body.appendChild(svg);
         var viewBox = svg.viewBox.baseVal;
         if (viewBox && (viewBox.x || viewBox.y)) {
-            svg.width.baseVal.value = viewBox.width - viewBox.x;
-            svg.height.baseVal.value = viewBox.height - viewBox.y;		
           if (svg.querySelector("text") == null) {	
-
+            svg.width.baseVal.value = viewBox.width - viewBox.x;
+            svg.height.baseVal.value = viewBox.height - viewBox.y;
             viewBox.x = 0;
             viewBox.y = 0;
             viewBox.width = 0;
