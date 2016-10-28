c5eed77b0117b22472a97bd361f49b378dc8940e
htmlgames <paul777@myway.com>
2016-10-28 08:33:43 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -2018,10 +2018,10 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
     if (this.textLayer) {
       this.context.drawImage(this.textLayer, 0, 0);
     }
-    if (this.index == this.base.currentCostumeIndex) { // this.base.isStage &&
+    if (this.base.isStage && (this.index == this.base.currentCostumeIndex)) { // this.base.isStage &&
       setTimeout(function() {
-        if (this.base.isStage) this.base.updateBackdrop();
-      }.bind(this)); // , 100) PF FF fix only, may not be required ?
+        this.base.updateBackdrop();
+      } //.bind(this)); // , 100) PF FF fix only, may not be required ?
     }
   };
 
