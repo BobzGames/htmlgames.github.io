3155171888f114354d859516c3378fed2703ab66
htmlgames <paul777@myway.com>
2016-10-28 08:44:09 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -237,8 +237,8 @@ var P = (function() {
       request.error(new Error('XHR Error'));
     };
     xhr.responseType = type || '';
-    //setTimeout(xhr.send.bind(xhr));
-    (function(){xhr.send.bind(xhr)})(); // pf
+    setTimeout(xhr.send.bind(xhr), 1);
+    //(function(){xhr.send.bind(xhr)})(); // pf
     if (callback) request.onLoad(callback.bind(self));
     return request;
   };
