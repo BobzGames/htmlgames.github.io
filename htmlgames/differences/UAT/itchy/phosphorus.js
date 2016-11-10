5480b3023341c859fe8511735bfc2de7d7be6635
htmlgames <paul777@myway.com>
2016-11-10 01:09:19 +0000

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1672,9 +1672,9 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
 	  effectsCanvas.width = costume.image.width;
 	  effectsCanvas.width = costume.image.height;
 
-          for (var o = 0; o < mosaicVal; o++) {
-            for (var i = 0; i < mosaicVal; i++) {
-              effectsContext.drawImage(costume.image, mosaicVal + (o * costume.image.width / mosaicVal), mosaicVal + (i * costume.image.height / mosaicVal), costume.image.width / mosaicVal, costume.image.height / mosaicVal);
+          for (var o = 0; o < effectsCanvas.width / mosaicVal; o++) {
+            for (var i = 0; i < effectsCanvas.width / mosaicVal; i++) {
+              effectsContext.drawImage(costume.image, o * costume.image.width / mosaicVal, i * costume.image.height / mosaicVal, costume.image.width / mosaicVal, costume.image.height / mosaicVal);
             }
 	  }
         }
