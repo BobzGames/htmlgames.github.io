87db5a8a292ac741dceff0e5b440aae526e173e6
htmlgames <paul777@myway.com>
2016-11-11 20:28:51 +0000

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1760,7 +1760,7 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
         }
 	    
         if (this.filters.brightness !== 0) {
-	  var brightnessVal = (this.filters.brightness / 2.55);
+	  var brightnessVal = (this.filters.brightness / 1);
 	
 	  effectsCanvas.width = costume.image.width;
 	  effectsCanvas.height = costume.image.height;		
