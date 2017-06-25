0ad30c0d322f56f5168a4f2323e2b62ced60ed49
htmlgames <paul777@myway.com>
2017-06-25 23:50:36 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,4 +1,4 @@
-// additional bugfixes by PF (v0.281)  < insert random number here...
+// additional bugfixes by PF (v0.283)  < insert random number here...
 // 
 // Sometimes, if this file is a certain size, Chrome 64bit on Windows 10 compiles it so it gives an extra, noticable speed boost (x2!)
 // But I don't know why? UPDATE: possible Chrome is switching gfx card from intel to nvidia...
@@ -1246,7 +1246,7 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
           var key = e.keyCode;
 	  //console.log(key); //
           e.stopPropagation();
-          if (e.target === this.canvas && !this.keys[key] && "17.37.38.39.40".match(key.toString())) { // db4
+          if (e.target === this.canvas && !this.keys[key] && "16.17.37.38.39.40".match(key.toString())) { // db4
 	    //if (key == 16) key = 0;
 	    //if (key == 17) key = 0;  
 	    if (key == 37) key = 28;
@@ -1676,7 +1676,7 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
 	    if (keyName && keyName.length > 0) {
 	      return KEY_CODES[keyName.toLowerCase()] || keyName.charCodeAt(0);
 	    } else {
-	      return 128;
+	      return -1;
 	    }
 	  } // pf db1 // pf temp - old code but tested 
        } else {
