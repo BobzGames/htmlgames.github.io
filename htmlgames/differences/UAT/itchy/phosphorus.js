55eefabfe3594a661be85ba6daf8ae5d8ba3a876
htmlgames <paul777@myway.com>
2017-07-13 23:32:15 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -803,14 +803,14 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
         document.body.appendChild(svg);
         var viewBox = svg.viewBox.baseVal;
         if (viewBox && (viewBox.x || viewBox.y)) {
-          //if (svg.querySelector("text") == null) { // PF fix svg image but not text nodes		
-          //  svg.width.baseVal.value = viewBox.width - viewBox.x;
-          //  svg.height.baseVal.value = viewBox.height - viewBox.y;	
-          //  viewBox.x = 0;
-          //  viewBox.y = 0;
-          //  viewBox.width = 0;
-          //  viewBox.height = 0;
-          //}
+          if (true && svg.querySelector("text") == null) { // PF fix svg image but not text nodes		
+            svg.width.baseVal.value = viewBox.width - viewBox.x;
+            svg.height.baseVal.value = viewBox.height - viewBox.y;	
+            viewBox.x = 0;
+            viewBox.y = 0;
+            viewBox.width = 0;
+            viewBox.height = 0;
+          }
           // SF
           var bb = svg.getBBox();
           viewBox.width  = svg.width.baseVal.value = Math.ceil(bb.x + bb.width + 1);
