0bfb587ab996c60da17e83f1d41aae8cca87f2a3
htmlgames <paul777@myway.com>
2017-07-15 00:28:02 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1481,10 +1481,6 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
     this.backdropContext.restore();
   };
 
-  // PF new
-  var effectsCanvasStage = document.createElement('canvas');
-  var effectsContextStage = effectsCanvasStage.getContext('2d');	
-	
   Stage.prototype.updateFilters = function() {
     this.backdropCanvas.style.opacity = Math.max(0, Math.min(1, 1 - this.filters.ghost / 100));
     // TOOO: add other effects... (warning will cause slowdown!)
@@ -1649,10 +1645,10 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
 	if (this.filters.color !== 0) {
 	  var colorVal = (this.filters.color * 2.55) & 0xff;
 	
-	  effectsCanvasStage.width = 480;
-	  effectsCanvasStage.height = 360;		
-	  effectsContextStage.drawImage(costume.image, 0, 0, 480, 360);
-	  var effect = effectsContextStage.getImageData(0, 0, 480, 360);
+	  effectsCanvas.width = 480;
+	  effectsCanvas.height = 360;		
+	  effectsContext.drawImage(costume.image, 0, 0, 480, 360);
+	  var effect = effectsContext.getImageData(0, 0, 480, 360);
           // PF: TODO improve
           for (var i = 0; i < effect.data.length; i += 4) {
             effect.data[i + 0] = (effect.data[i + 0] + colorVal) & 0xff;
@@ -1660,11 +1656,11 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
             effect.data[i + 2] = (effect.data[i + 2] + colorVal) & 0xff;
             effect.data[i + 3] = effect.data[i + 3]; // alpha
 	  }
-	  effectsContextStage.putImageData(effect, 0, 0);
+	  effectsContext.putImageData(effect, 0, 0);
         }
 	// TODO: others ...
 	  
-	context.drawImage(effectsCanvasStage, 0, 0, 480, 360); // was this.backdropContext       
+	context.drawImage(effectsCanvas, 0, 0, 480, 360); // was this.backdropContext       
     } else {		  
       context.drawImage(costume.image, 0, 0);
     }
@@ -4750,6 +4746,7 @@ P.runtime = (function() {
       this.promptId = this.nextPromptId = 0;
       this.queue = [];
       this.resetFilters();
+      this.updateFilters(); // pf
       this.stopSounds();
       for (var i = 0; i < this.children.length; i++) {
         var c = this.children[i];
