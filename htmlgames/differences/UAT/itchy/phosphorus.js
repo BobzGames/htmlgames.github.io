00ce5243af2d934e08e637027213095f8143da60
htmlgames <paul777@myway.com>
2017-07-15 02:40:31 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -962,7 +962,7 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
 
   Base.prototype.showList = function(name) {
      console.log("Show List:" + name);
-     var o_list = getLists(name);
+     var o_list = this.getLists(name);
      //console.log();
   };
 	
