f0ecd489612edc8e5644bd91201d266b97b82892
htmlgames <paul777@myway.com>
2017-07-23 13:25:05 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1144,10 +1144,10 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
           // PF: TODO improve
           for (var i = 0; i < effect.data.length; i += 4) {
 	    // cheap 'n' nasty way...
-            if (effect.data[i + 0] > 0 ) effect.data[i + 0] = (effect.data[i + 0] + colorVal) & 0xff;
-            if (effect.data[i + 1] > 0 ) effect.data[i + 1] = (effect.data[i + 1] + colorVal) & 0xff;
-            if (effect.data[i + 2] > 0 ) effect.data[i + 2] = (effect.data[i + 2] + colorVal) & 0xff;
-            if (effect.data[i + 3] > 0 ) effect.data[i + 3] = effect.data[i + 3]; // alpha
+            if (effect.data[i + 0] > 8 && effect.data[i + 0] < 128) effect.data[i + 0] = (effect.data[i + 0] + colorVal) & 0xff;
+            if (effect.data[i + 1] > 8 && effect.data[i + 0] < 128 ) effect.data[i + 1] = (effect.data[i + 1] + colorVal) & 0xff;
+            if (effect.data[i + 2] > 8 && effect.data[i + 0] < 128 ) effect.data[i + 2] = (effect.data[i + 2] + colorVal) & 0xff;
+            if (effect.data[i + 3] > 8 && effect.data[i + 0] < 128 ) effect.data[i + 3] = effect.data[i + 3]; // alpha
 		  
 	    // (offical) sulfurous way...
 	    //var colorOld = rgbToHsv(effect.data[i + 0], effect.data[i + 1], effect.data[i + 2]);
