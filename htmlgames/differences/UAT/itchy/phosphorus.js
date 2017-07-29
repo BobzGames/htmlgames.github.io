a144c32fb8bc8b2a71c09ba6c324e532df0cdcae
htmlgames <paul777@myway.com>
2017-07-29 22:34:47 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -428,7 +428,7 @@ var P = (function() {
   IO.wavBuffers = Object.create(null);
   IO.loadWavs = function() {
     if (!audioContext) return;
-    if (true || location.hash.substr(1) === 'zip') return; // pf temp test
+    if (location.hash.substr(1) === 'zip') return; // pf temp test - need to load local files here...
 	  
     for (var name in wavFiles) {
       if (IO.wavBuffers[name]) {
