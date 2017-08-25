19fed2b7ae4a933acc5d2cda365f19109a427bec
htmlgames <paul777@myway.com>
2017-08-26 00:12:56 +0100

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1304,7 +1304,7 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
           // PF: TODO improve 172233660
           for (var i = 0; i < effect.data.length; i += 4) {
 	    if (effect.data[i + 0] + effect.data[i + 1] + effect.data[i + 2]) { // ignore black #000
-	      if (effect.data[i + 0] + effect.data[i + 1] + effect.data[i + 2] < brightnessVal) {
+	      if (effect.data[i + 0] + effect.data[i + 1] + effect.data[i + 2] > brightnessVal) {
                 effect.data[i + 0] = (effect.data[i + 0] + brightnessVal);
                 effect.data[i + 1] = (effect.data[i + 1] + brightnessVal);
                 effect.data[i + 2] = (effect.data[i + 2] + brightnessVal);
