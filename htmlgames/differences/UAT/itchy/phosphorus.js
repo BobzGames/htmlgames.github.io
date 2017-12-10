3d954d0591366b395d1f46f0d83b9d593b3a40d5
htmlgames <paul777@myway.com>
2017-12-10 19:37:27 +0000

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,6 +1,6 @@
 // Scratch2apk: An (almost complete) scratch emulator written in javascript - includes support for (some) hacked blocks 
 //
-// (v0.317) < insert random number here...
+// (v0.319) < insert random number here...
 //
 // Based on phosphorus (phosphorus.github.io) with additional bugfixes and enhancements by PF 
 //
@@ -3289,13 +3289,16 @@ P.compile = (function() {
 
     var param = function(name, usenum, usebool) {
       if (typeof name !== 'string') {
-	// try to see if hacked block !here
-	if (typeof block[1] === 'object' && block[1].length === 3 && block[1][0] === 'getParam' && block[1][1] === 'Var' && typeof block[1][2] === 'string') {
-	   block[1] = block[1][2]; // danger! assumes...
-	} else {
-           throw new Error('[Hacked Block] Dynamic parameters unknown');
-	}
-      }
+        throw new Error('Dynamic parameters are not supported');
+      }	    
+      //if (typeof name !== 'string') {
+	// try to see if hacked block !here TODO
+	//if (typeof name[1] === 'object' && name[1].length === 3 && name[1][0] === 'getParam' && name[1][1] === 'Var' && typeof name[1][2] === 'string') {
+	   //name[1] = name[1][2]; // danger! assumes...
+	//} else {
+           //throw new Error('[Hacked Block] Dynamic parameters unknown');
+	//}
+      //}
 
       if (!inputs) return '0';
 
