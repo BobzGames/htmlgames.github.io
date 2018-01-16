2827c9aa6fec2ba11d7c0eca8802ed198667d8f3
htmlgames <paul777@myway.com>
2018-01-16 23:56:10 +0000

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,6 +1,6 @@
 // Scratch2apk: An (almost complete) scratch emulator written in javascript - includes support for (some) hacked blocks 
 //
-// (v0.324) < insert random number here... 
+// (v0.323) < insert random number here...
 //
 // Based on phosphorus (phosphorus.github.io) with additional bugfixes and enhancements by PF 
 //
@@ -1779,7 +1779,7 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
 	divContainer.style.overflow = 'hidden';
 	divContainer.style.left = info[0] + 'em';
 	divContainer.style.top = info[1] + 'em';
-	divContainer.style.width = (info[2]-8) + 'em';	    
+	divContainer.style.width = info[2] + 'em'; // TODO: if left + width > 480 then adjust to be < 480	    
 	if (o_list.length) divContainer.style.height = info[3] + 'em';
 	divContainer.innerHTML = "<div style='margin: 2em'><span style='font-size: 12em; text-align: center; font-weight: bold;'><center>" + name + "</center></span></div>";
 	    
@@ -1794,7 +1794,7 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
 	for (var i = 0; i < o_list.length; i++) { // test
 	  divItem = document.createElement('div');
 	  divItem.style.backgroundColor = "#c1c4c7";
-	  divItem.innerHTML = "<input readonly value=' " + (i + 1) + "' style='color: #000; border: 0; background-color: #c1c4c7; width: 10%; font-size: 11em; margin: 1px'/> <input readonly value='" + o_list[i].replace(/'/g, "&#39;") + "' style='font-size: 11em; background-color: #cc5b22; color: white; width: 64%; height: 1em; border: 1px solid #fff; border-radius: 3px; padding: 3px; margin: 0px;' />";
+	  divItem.innerHTML = "<input readonly value=' " + (i + 1) + "' style='color: #000; border: 0; background-color: #c1c4c7; width: 10%; font-size: 11em; margin: 1px'/> <input readonly value='" + o_list[i].replace(/'/g, "&#39;") + "' style='font-size: 11em; background-color: #cc5b22; color: white; width: 75%; height: 1em; border: 1px solid #fff; border-radius: 3px; padding: 3px; margin: 0px;' />"; // TODO: rid 75% width and calc instead!
 	  divInner.appendChild(divItem);	
 	}
 	    
