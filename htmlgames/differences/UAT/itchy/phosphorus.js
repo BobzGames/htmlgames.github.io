5ea60a64946c72dc33131a38d035e14682b8509b
htmlgames <paul777@myway.com>
2018-01-23 22:23:07 +0000

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1811,10 +1811,10 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
 // qtest
 		console.log("HEIGHT="+info[3]);
 		divItem = document.createElement('div');
-		divItem.innerHTML = "<p style='padding-top: 86%; border: 1px solid red'><p style='font-size: 11em; text-align: center;'>(empty)</p></p>"; // 
+		divItem.innerHTML = "<p style='padding-top: 86%; border: 1px solid red'><p style='padding-bottom: 50%; border: 1px solid green; font-size: 11em; text-align: center;'>(empty)</p></p><div style='font-size: 11em; text-align: center; padding-bottom: 2em'>length: 0</div>"; // 
 		divItem.style.height = ( (info[3] / 100) * 93 ) + 0 + 'em'; // em%
 		divInner.appendChild(divItem);
-		divItem2.innerHTML = "<div style='font-size: 11em; text-align: center; padding-bottom: 2em'>length: 0</div>";
+		
 	  //divItem2.innerHTML = "<p><p style='font-size: 11em; text-align: center;'>(empty)</p></p><div style='font-size: 11em; text-align: center; padding-bottom: 2em'>length: 0</div>";
 	}
 	divHolder.appendChild(divInner);
