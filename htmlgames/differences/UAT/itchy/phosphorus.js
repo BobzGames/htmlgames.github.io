a52fe02a66d4ea5c9b7126e36443f259fc10f0ed
htmlgames <paul777@myway.com>
2018-02-04 21:03:12 +0000

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -2605,12 +2605,12 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
       collisionContext.translate(-(240 + b.left), -(180 - b.top));
       this.stage.drawOn(collisionContext, this);	    
     } else {
-      //collisionContext.save();
+      collisionContext.save(); // ! needed?
       collisionContext.translate(-(240 + b.left), -(180 - b.top));
       this.stage.drawAllOn(collisionContext, this);
       collisionContext.globalCompositeOperation = 'destination-in';
       this.draw(collisionContext, true);	  
-      //collisionContext.restore();
+      collisionContext.restore(); // ! needed?
     }
 	  
     var wt = (w < 1) ? 1 : w;
