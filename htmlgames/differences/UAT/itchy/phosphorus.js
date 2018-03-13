63a6fd725ecfbbbc44f74e8be6cff4c8b34f74ab
htmlgames <paul777@myway.com>
2018-03-13 23:37:52 +0000

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1184,6 +1184,7 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
     switch (name) {
       case 'whirl':
       case 'fisheye':
+      case 'brightness':
       case 'pixelate': // absolute value
       case 'mosaic': // absolute value
         min = -Infinity;
