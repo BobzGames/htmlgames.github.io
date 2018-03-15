c650a0c88ce0c157ba393690d576f0bee527856c
htmlgames <paul777@myway.com>
2018-03-15 20:21:36 +0000

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -1,7 +1,7 @@
 // Scratch2apk: An (almost complete) scratch emulator written in javascript - includes support for (some) hacked blocks 
 //
 // (v0.239C) < insert random number here... C = Cloud variables inspired by http://phosphate.herokuapp.com/
-var LAKITU = !false; 
+var LAKITU = !false;
 //
 // Based on phosphorus (phosphorus.github.io) with additional bugfixes and enhancements by PF 
 //
