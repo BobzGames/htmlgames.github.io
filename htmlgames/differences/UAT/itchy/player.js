96b2b8bc234c15a7c500310547567aca576e3654
htmlgames <paul777@myway.com>
2018-03-20 00:41:13 +0000

    Update player.js

--- htmlgames/differences/UAT/itchy/player.js
+++ htmlgames/differences/UAT/itchy/player.js
@@ -267,8 +267,8 @@ P.player = (function() {
 
       var zoom = stage ? stage.zoom : 1;
       window.stage = stage = s;
+      stage.setZoom(zoom); // PF move up 1 line    
       stage.start();
-      stage.setZoom(zoom);
 
       stage.root.addEventListener('keydown', exitFullScreen);
       stage.handleError = showError;
