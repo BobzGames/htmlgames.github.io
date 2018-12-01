fade88e3085a659de301e078e8b498ac1c091f05
htmlgames <paul777@myway.com>
2018-12-01 01:17:06 +0000

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -2517,7 +2517,7 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
     var d = (90 - this.direction) * Math.PI / 180;
     this.moveTo(this.scratchX + steps * Math.cos(d), this.scratchY + steps * Math.sin(d));
   };
-  var gwiz = 1920;
+  var gwiz = 480 * 100;
   Sprite.prototype.moveTo = function(x, y) {
     var ox = this.scratchX;
     var oy = this.scratchY;
