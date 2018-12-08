b678b134be13308cdc203bca8df2f52e718d4bb1
htmlgames <paul777@myway.com>
2018-12-08 20:47:13 +0000

    Update phosphorus.js

--- htmlgames/differences/UAT/itchy/phosphorus.js
+++ htmlgames/differences/UAT/itchy/phosphorus.js
@@ -2527,6 +2527,7 @@ function encodeAudio16bit(soundData, sampleRate, soundBuf) {
     if (ox === x && oy === y && !this.isPenDown) return;
 	  
     // new condition block below - fixes:133622642
+    if (x >= gwix) gwiDisable = false; 
     if (gwiDisable||Math.abs(x) < gwix) {
       this.scratchX = x; // ### pft2
     } else {
